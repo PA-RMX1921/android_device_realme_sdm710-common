b00d6de21ece27de8135076da7216e0571165558
rahulhingve <rahulpawar2001.rp@gmail.com>
2020-04-11 11:30:29 +0000

    sdm710-common: Remove sepolicy include
    its getting included automatically
    
    Signed-off-by: rahulhingve <rahulpawar2001.rp@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -169,7 +169,6 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2019-08-05
 
 # Sepolicy
-include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
