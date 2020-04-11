0cb8bc4aa575325adf5293ee3905a142655a0286
rahulhingve <rahulpawar2001.rp@gmail.com>
2020-04-11 10:35:47 +0000

    sdm710-common: Remove lineage specific stuffs
    Signed-off-by: rahulhingve <rahulpawar2001.rp@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -15,9 +15,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product-if-exists, vendor/realme/sdm710-common/sdm710-common-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_props.mk
@@ -392,10 +390,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sdm710
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
